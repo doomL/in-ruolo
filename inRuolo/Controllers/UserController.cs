25c6877cb88ec6732fb8cbfb9d86aab1850d866f
Domenico Laurito <laurito.dom@gmail.com>
2020-10-13 15:48:52 +0200

    Initial Commit, UserAdmin Pages

---
+++ inRuolo/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inRuolo.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Fatturazione()
+        {
+            return View();
+        }
+        public ActionResult CambiaPassword()
+        {
+            return View();
+        }
+        public ActionResult Formazione()
+        {
+            return View();
+        }
+        
+
+    }
+}
